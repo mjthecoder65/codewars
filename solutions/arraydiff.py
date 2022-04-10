21a9e6b28c1d9199d42c0b7a0f562cd9c4795ffa
Michael Jordan <mjthecoder53161@gmail.com>
2022-04-10 17:49:50 +0900

    Initial commit

---
+++ solutions/arraydiff.py
@@ -0,0 +1,24 @@
+# def array_diff(a, b):
+#     if len(b) == 0:
+#         return a
+#     diff = []
+#     for number in a:
+#         if number not in b:
+#             diff.append(number)
+#     return diff
+
+def array_diff(a, b):
+    if len(b) == 0:
+        return a
+    checkpoint = set() # O(N)
+    for number in b:
+        checkpoint.add(number)
+    difference = []
+    for number in a:
+        if number not in checkpoint:
+            difference.append(number)
+
+    return difference
+
+
+__all__ = ['array_diff']
---
+++ tinker.py
@@ -0,0 +1,8 @@
+numbers1 = [2, 3]
+numbers2 = [3, 2]
+
+
+if numbers1 == numbers2:
+    print("They are equal")
+else:
+    print("They are not equal")
